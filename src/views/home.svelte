926725e0a80f5f6518b91e72e147d6aaea8ba404
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2021-10-29 18:59:33 +1100

    Refresh the Recently Played list on homepage when song changes

--- src/views/home.svelte
+++ src/views/home.svelte
@@ -1,13 +1,17 @@
 <script>
     import CardList from '../components/cardList.svelte';
 
+    import { CurrentSong } from "../stores/status";
+
     import { recentSongs } from "../logic/song";
     import { newestAlbums, randomAlbums } from "../logic/album";
 </script>
 
 <CardList type="album" dataProvider={"newestAlbums"} limit=12 containerType="scroll" heading="Newest albums" />
 
-<CardList type="song" dataProvider={"recentSongs"} limit=9 containerType="scroll" heading="Recently played" />
+{#key ($CurrentSong) ? $CurrentSong.id : 0}
+    <CardList type="song" dataProvider={"recentSongs"} limit=9 containerType="scroll" heading="Recently played" />
+{/key}
 
 <CardList type="album" dataProvider={"randomAlbums"} limit=12 refresh=true containerType="scroll" heading="Random albums" />
 
