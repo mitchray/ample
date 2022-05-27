6e56335fae1e9f0ced38f244e8f5c09002c27f2b
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2022-05-27 19:00:11 +1000

    'Fancy' searching from quick search

--- src/components/search.svelte
+++ src/components/search.svelte
@@ -4,13 +4,13 @@
     import { SearchQuery, ShowSearch } from '../stores/status';
     import { SiteContentBind } from '../stores/player';
 
-    import { searchArtists } from '../logic/artist';
-    import { searchAlbums } from '../logic/album';
-    import { searchSongs } from '../logic/song';
-    import { searchPlaylists } from '../logic/playlist';
-    import { searchSmartlists } from '../logic/playlist';
+    import { searchArtists, getArtistsFromAdvancedSearch } from '../logic/artist';
+    import { searchAlbums, getAlbumsFromAdvancedSearch } from '../logic/album';
+    import { searchSongs, getSongsFromAdvancedSearch } from '../logic/song';
+    import { searchPlaylists, searchSmartlists } from '../logic/playlist';
 
     import CardList from './cardList.svelte';
+    import Lister2 from '../components/lister/lister.svelte';
 
     import SVGClose from "../../public/images/close.svg";
 
@@ -18,6 +18,10 @@
     let noResults = false;
     let isExact = false;
     let queryWithoutQuotes = "";
+    let fancySearchResults = {};
+    let nothingFancy = true;
+
+    let regexKeywords = "artist|album|song|title|playlist|smartlist|$";
 
     function handleClick(event) {
         // Close search if we are following a link
@@ -32,10 +36,18 @@
 
     $: {
         if ($SearchQuery) {
-            isExact = new RegExp(/^".*"$/gi).test($SearchQuery);
-            queryWithoutQuotes = $SearchQuery.replaceAll(/(^"|"$)/ig, '');
-
             resetResults();
+
+            // parse fancy items from string
+            fancySearchResults.artist    = $SearchQuery.match(new RegExp("(?<=artist:).+?(?=" + regexKeywords + ")", "gi"));
+            fancySearchResults.album     = $SearchQuery.match(new RegExp("(?<=album:).+?(?=" + regexKeywords + ")", "gi"));
+            fancySearchResults.song      = $SearchQuery.match(new RegExp("(?<=(song|title):).+?(?=" + regexKeywords + ")", "gi"));
+
+            // test if every array in fancySearchResults is null
+            nothingFancy = Object.values(fancySearchResults).every(value => {
+                return value === null;
+            });
+
             doSearch();
         }
     }
@@ -48,14 +60,101 @@
             playlists: [],
             smartlists: [],
         };
+
+        fancySearchResults = {
+            artist: null,
+            album: null,
+            song: null
+        };
     }
 
     async function doSearch() {
-        const s1 = searchArtists({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
-        const s2 = searchAlbums({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
-        const s3 = searchSongs({query: queryWithoutQuotes, page: 0, limit: 9, exact: isExact});
-        const s4 = searchPlaylists({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
-        const s5 = searchSmartlists({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
+        let s1 = [];
+        let s2 = [];
+        let s3 = [];
+        let s4 = [];
+        let s5 = [];
+        let rows = [];
+        let counter = 0;
+
+        if (nothingFancy) {
+            isExact = new RegExp(/^".*"$/gi).test($SearchQuery);
+            queryWithoutQuotes = $SearchQuery.replaceAll(/(^"|"$)/ig, '');
+
+            s1 = searchArtists({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
+            s2 = searchAlbums({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
+            s3 = searchSongs({query: queryWithoutQuotes, page: 0, limit: 9, exact: isExact});
+            s4 = searchPlaylists({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
+            s5 = searchSmartlists({query: queryWithoutQuotes, page: 0, limit: 6, exact: isExact});
+        } else {
+            if (fancySearchResults.song) {
+                rows.push({
+                    field: "title",
+                    id: ++counter,
+                    input: fancySearchResults.song,
+                    inputType: "text",
+                    operator: "0",
+                    operatorType: "string",
+                });
+
+                if (fancySearchResults.artist) {
+                    rows.push({
+                        field: "artist",
+                        id: ++counter,
+                        input: fancySearchResults.artist,
+                        inputType: "text",
+                        operator: "0",
+                        operatorType: "string",
+                    })
+                }
+
+                if (fancySearchResults.album) {
+                    rows.push({
+                        field: "album",
+                        id: ++counter,
+                        input: fancySearchResults.album,
+                        inputType: "text",
+                        operator: "0",
+                        operatorType: "string",
+                    });
+                }
+
+                s3 = getSongsFromAdvancedSearch({rows: rows, limit: 100, random: false, match: "all"});
+            } else if (fancySearchResults.album) {
+                rows.push({
+                    field: "title",
+                    id: ++counter,
+                    input: fancySearchResults.album,
+                    inputType: "text",
+                    operator: "0",
+                    operatorType: "string",
+                });
+
+                if (fancySearchResults.artist) {
+                    rows.push({
+                        field: "artist",
+                        id: ++counter,
+                        input: fancySearchResults.artist,
+                        inputType: "text",
+                        operator: "0",
+                        operatorType: "string",
+                    })
+                }
+
+                s2 = getAlbumsFromAdvancedSearch({rows: rows, limit: 100, random: false, match: "all"});
+            } else if (fancySearchResults.artist) {
+                rows.push({
+                    field: "title",
+                    id: ++counter,
+                    input: fancySearchResults.artist,
+                    inputType: "text",
+                    operator: "0",
+                    operatorType: "string",
+                });
+
+                s1 = getArtistsFromAdvancedSearch({rows: rows, limit: 100, random: false, match: "all"});
+            }
+        }
 
         [initialResults.artists, initialResults.albums, initialResults.songs, initialResults.playlists, initialResults.smartlists] = await Promise.all([s1, s2, s3, s4, s5]);
 
@@ -84,15 +183,27 @@
                 {/if}
 
                 {#if initialResults.artists.length > 0}
-                    <CardList type="artist" initialData={initialResults.artists} dataProvider={"searchArtists"} limit=6 arg={encodeURI($SearchQuery)} heading="Artists" />
+                    {#if nothingFancy}
+                        <CardList type="artist" initialData={initialResults.artists} dataProvider={"searchArtists"} limit=6 arg={encodeURI($SearchQuery)} heading="Artists" />
+                    {:else}
+                        <Lister2 data={initialResults.artists} type="artist" />
+                    {/if}
                 {/if}
 
                 {#if initialResults.albums.length > 0}
-                    <CardList type="album" initialData={initialResults.albums} dataProvider={"searchAlbums"} limit=6 arg={encodeURI($SearchQuery)} heading="Albums" />
+                    {#if nothingFancy}
+                        <CardList type="album" initialData={initialResults.albums} dataProvider={"searchAlbums"} limit=6 arg={encodeURI($SearchQuery)} heading="Albums" />
+                    {:else}
+                        <Lister2 data={initialResults.albums} type="album" />
+                    {/if}
                 {/if}
 
                 {#if initialResults.songs.length > 0}
-                    <CardList type="song" initialData={initialResults.songs} dataProvider={"searchSongs"} limit=9 arg={encodeURI($SearchQuery)} heading="Songs" />
+                    {#if nothingFancy}
+                        <CardList type="song" initialData={initialResults.songs} dataProvider={"searchSongs"} limit=9 arg={encodeURI($SearchQuery)} heading="Songs" />
+                    {:else}
+                        <Lister2 data={initialResults.songs} type="song" />
+                    {/if}
                 {/if}
 
                 {#if initialResults.playlists.length > 0}
