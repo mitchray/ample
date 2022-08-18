761788c396c96a5cd58125ef317bb6b4d31eb993
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2022-08-18 17:46:51 +1000

    Don't include sourceMap for builds

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ function serve() {
 export default {
 	input: 'src/main.ts',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
 		file: 'public/build/bundle.js',
