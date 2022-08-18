0d01836ab9b0f095c861ce189191b25734170858
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2022-08-18 16:34:28 +1000

    Show index on Smartlists

--- src/views/playlist.svelte
+++ src/views/playlist.svelte
@@ -100,7 +100,7 @@
 
                 <div class="songs-container">
                     {#key loadedTime}
-                        <Lister2 bind:data={songs} type="playlist_songs" showCheckboxes={!isSmartlist} showIndex={!isSmartlist} />
+                        <Lister2 bind:data={songs} type="playlist_songs" showCheckboxes={!isSmartlist} showIndex={true} />
                     {/key}
                 </div>
             </div>
