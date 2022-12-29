f4139b49644e4e5659fa69d219948b02417cecc1
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2022-12-29 14:43:34 +1100

    Loading

--- src/components/siteLoading.svelte
+++ src/components/siteLoading.svelte
@@ -1,3 +1,21 @@
-<div class="site-loading">
-    <p>Loading</p>
+<script>
+    import { fade } from "svelte/transition";
+</script>
+
+<div class="site-loading" out:fade>
+    Loading
 </div>
+
+<style>
+    .site-loading {
+        height: 100vh;
+        width: 100vw;
+        background-color: var(--color-background);
+        z-index: 9999;
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+    }
+</style>
