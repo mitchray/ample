b4e09b80344e896d0072d329cbe9db6b4279d7af
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2023-05-26 22:39:46 +1000

    Newest lister

--- src/views/newest.svelte
+++ src/views/newest.svelte
@@ -1,6 +1,7 @@
 <script>
-    import CardList from '../components/cardList.svelte';
     import { PageTitle } from "../stores/status";
+    import { newestAlbums } from "../logic/album"
+    import Lister2 from '../components/lister/lister.svelte';
 
     let title = "Newest";
     $PageTitle = title;
@@ -11,5 +12,25 @@
 </svelte:head>
 
 <div class="page-main">
-    <CardList type="album" dataProvider={"newestAlbums"} limit=18 />
+    {#await newestAlbums({limit: 100})}
+        Loading albums
+    {:then albums}
+        {#if albums.length > 0}
+            <Lister2
+                data={albums}
+                type="album"
+                virtualList={true}
+                actionData={{
+                    type: "albums",
+                    mode: "fullButtons",
+                    showShuffle: albums.length > 1,
+                    data: Object.create({albums: albums})
+                }}
+            />
+        {:else}
+            <p>No albums found</p>
+        {/if}
+    {:catch error}
+        <p>An error occurred.</p>
+    {/await}
 </div>
