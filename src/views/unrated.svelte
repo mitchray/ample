63f4156933a8d639758736e19eef77b728d16981
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2023-05-26 23:01:45 +1000

    Unrated lister

--- src/views/unrated.svelte
+++ src/views/unrated.svelte
@@ -1,9 +1,11 @@
 <script>
-    import CardList from '../components/cardList.svelte';
+    import { PageTitle } from "../stores/status";
+    import Lister2 from '../components/lister/lister.svelte';
+    import { unratedArtists } from "../logic/artist";
+    import { unratedAlbums } from "../logic/album";
+    import { unratedSongs } from "../logic/song";
     import Tabs from "../components/tabs/tabs.svelte";
     import Tab from "../components/tabs/tab.svelte";
-    import { PageTitle } from "../stores/status";
-
     import SVGArtist from "/src/images/artist.svg";
     import SVGAlbum from "/src/images/album.svg";
     import SVGSong from "/src/images/music_note.svg";
@@ -30,19 +32,79 @@
         {#if tab.loaded === true}
             {#if tab.value === 'artists'}
                 <Tab id="artists" class="artists" bind:activeTabValue={currentTab}>
-                    <CardList type="artist" dataProvider={"unratedArtists"} limit=6 refresh={true} />
+                    {#await unratedArtists({limit: 100})}
+                        Loading unrated artists
+                    {:then artists}
+                        {#if artists.length > 0}
+                            <Lister2
+                                data={artists}
+                                type="artist"
+                                virtualList={true}
+                                actionData={{
+                                    type: "artists",
+                                    mode: "fullButtons",
+                                    showShuffle: artists.length > 1,
+                                    data: Object.create({artists: artists})
+                                }}
+                            />
+                        {:else}
+                            <p>No artists found</p>
+                        {/if}
+                    {:catch error}
+                        <p>An error occurred.</p>
+                    {/await}
                 </Tab>
             {/if}
 
             {#if tab.value === 'albums'}
                 <Tab id="albums" class="albums" bind:activeTabValue={currentTab}>
-                    <CardList type="album" dataProvider={"unratedAlbums"} limit=6 refresh={true} />
+                    {#await unratedAlbums({limit: 100})}
+                        Loading unrated albums
+                    {:then albums}
+                        {#if albums.length > 0}
+                            <Lister2
+                                data={albums}
+                                type="album"
+                                virtualList={true}
+                                actionData={{
+                                    type: "albums",
+                                    mode: "fullButtons",
+                                    showShuffle: albums.length > 1,
+                                    data: Object.create({albums: albums})
+                                }}
+                            />
+                        {:else}
+                            <p>No albums found</p>
+                        {/if}
+                    {:catch error}
+                        <p>An error occurred.</p>
+                    {/await}
                 </Tab>
             {/if}
 
             {#if tab.value === 'songs'}
                 <Tab id="songs" class="songs" bind:activeTabValue={currentTab}>
-                    <CardList type="song" dataProvider={"unratedSongs"} limit=9 refresh={true} />
+                    {#await unratedSongs({limit: 100})}
+                        Loading unrated songs
+                    {:then songs}
+                        {#if songs.length > 0}
+                            <Lister2
+                                data={songs}
+                                type="song"
+                                virtualList={true}
+                                actionData={{
+                                    type: "",
+                                    mode: "fullButtons",
+                                    showShuffle: songs.length > 1,
+                                    data: Object.create({songs: songs})
+                                }}
+                            />
+                        {:else}
+                            <p>No songs found</p>
+                        {/if}
+                    {:catch error}
+                        <p>An error occurred.</p>
+                    {/await}
                 </Tab>
             {/if}
         {/if}
