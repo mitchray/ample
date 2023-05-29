b61266afec498dfbed0059695ae7cd7be1e0d906
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2023-05-29 23:13:48 +1000

    Also don't need pagination

--- src/components/genre/genreByType.svelte
+++ src/components/genre/genreByType.svelte
@@ -1,27 +1,16 @@
 <script>
+    import { onMount } from "svelte";
     import { API } from "../../stores/api";
     import Lister2 from '../../components/lister/lister.svelte';
-    import Pagination from '../../components/Pagination.svelte';
 
     export let id;
     export let type;
 
     let genre;
     let data = [];
-    let defaultLimit = 50;
-    let page = 0;
-    let count = 0;
     let loadedTime = 0;
-    let limit = 0;
 
-    $: count = data.length || 0;
     $: data = data;
-
-    $: {
-        if (limit > 0 || page) {
-            getData();
-        }
-    }
 
     async function getData() {
         genre = await $API.genre({ filter: id });
@@ -29,13 +18,13 @@
         if (id && genre.id) {
             switch (type) {
                 case "artist":
-                    data = await $API.genreArtists({ filter: id, limit: limit, offset: limit * page });
+                    data = await $API.genreArtists({ filter: id, limit: 100 });
                     break;
                 case "album":
-                    data = await $API.genreAlbums({ filter: id, limit: limit, offset: limit * page });
+                    data = await $API.genreAlbums({ filter: id, limit: 100 });
                     break;
                 case "song":
-                    data = await $API.genreSongs({ filter: id, limit: limit, offset: limit * page });
+                    data = await $API.genreSongs({ filter: id, limit: 100 });
                     break;
                 default:
                     break;
@@ -44,53 +33,54 @@
 
         loadedTime = new Date();
     }
+
+    onMount(async () => {
+        await getData();
+    });
 </script>
 
-{#await $API.genre({ filter: id })}
-    Loading
-{:then genre}
-    {#if genre.id}
-        {#key loadedTime}
-            {#if type === 'artist'}
-                <Lister2
-                    data={data}
-                    type="artist"
-                    activeSort="title"
-                    actionData={{
-                        type: "artistGenre",
-                        mode: "fullButtons",
-                        data: Object.create({name: genre.name})
-                    }}
-                />
-            {/if}
+{#key loadedTime}
+    {#if genre?.name && data}
+        {#if type === 'artist'}
+            <Lister2
+                data={data}
+                type="artist"
+                initialSort="title"
+                virtualList={true}
+                actionData={{
+                    type: "artistGenre",
+                    mode: "fullButtons",
+                    data: Object.create({name: genre.name})
+                }}
+            />
+        {/if}
 
-            {#if type === 'album'}
-                <Lister2
-                    data={data}
-                    type="album"
-                    activeSort="title"
-                    actionData={{
-                        type: "albumGenre",
-                        mode: "fullButtons",
-                        data: Object.create({name: genre.name})
-                    }}
-                />
-            {/if}
+        {#if type === 'album'}
+            <Lister2
+                data={data}
+                type="album"
+                initialSort="title"
+                virtualList={true}
+                actionData={{
+                    type: "albumGenre",
+                    mode: "fullButtons",
+                    data: Object.create({name: genre.name})
+                }}
+            />
+        {/if}
 
-            {#if type === 'song'}
-                <Lister2
-                    data={data}
-                    type="song"
-                    activeSort="title"
-                    actionData={{
-                        type: "songGenre",
-                        mode: "fullButtons",
-                        data: Object.create({name: genre.name})
-                    }}
-                />
-            {/if}
-        {/key}
+        {#if type === 'song'}
+            <Lister2
+                data={data}
+                type="song"
+                initialSort="title"
+                virtualList={true}
+                actionData={{
+                    type: "songGenre",
+                    mode: "fullButtons",
+                    data: Object.create({name: genre.name})
+                }}
+            />
+        {/if}
     {/if}
-{:catch error}
-    <p>An error occurred.</p>
-{/await}
+{/key}
