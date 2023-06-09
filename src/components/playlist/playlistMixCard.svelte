f7828233ff4763be99b97245013c6145c6353fab
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2023-06-09 20:37:36 +1000

    Leave out 'mix'

--- src/components/playlist/playlistMixCard.svelte
+++ src/components/playlist/playlistMixCard.svelte
@@ -31,7 +31,7 @@
     {#if playlist?.id}
         <div class="image-container">
             <Link to="mix/{playlist.playlistType}/{playlist.id}" title="{playlist.name}">
-                <div class="image-text">{playlist.name} <span class="mix-text">{$_('text.mix')}</span></div>
+                <div class="image-text">{playlist.name}</div>
                 <img class="image"
                      src="{cleanArtURL(playlist.art)}&thumb=22"
                      alt="Image of {playlist.name}"
@@ -175,10 +175,6 @@
                 var(--color-mix-background)
         );
         z-index: -1;
-    }
-
-    .mix-text {
-        font-weight: 400;
     }
 
     .details {
