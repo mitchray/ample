a424315fb62e97f44f817fecc42c8a67ea3b066c
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2024-06-30 10:16:02 +1000

    Favourites remote pagination

--- src/views/favorites/artists.svelte
+++ src/views/favorites/artists.svelte
@@ -1,61 +1,50 @@
 <script>
     import Tabulator from "~/components/lister/Tabulator.svelte";
-    import Actions from "~/components/action/actions.svelte";
     import MassRater from "~/components/lister/massRater.svelte";
-    import { favoriteArtists } from "~/logic/artist.js";
-    import { errorHandler } from "~/logic/helper.js";
-    import { _ } from "svelte-i18n";
-    import { createQuery } from "@tanstack/svelte-query";
-    import { User } from "~/stores/state.js";
-    import { artistsPreset } from "~/components/lister/columns.js";
+    import { API, User } from "~/stores/state.js";
+    import {
+        artistsPreset,
+        normalizeResponse,
+        remotePaginationDefaults,
+    } from "~/components/lister/columns.js";
 
     let tabulator = null;
-
-    $: query = createQuery({
-        queryKey: ["favoriteArtists"],
-        queryFn: async () => {
-            let result = await favoriteArtists({ limit: 5000 });
-
-            if (result.error) {
-                errorHandler("getting favorite artists", result.error);
-                return [];
-            }
-
-            return result;
-        },
-        enabled: $User.isLoggedIn,
-    });
-
-    // alias of returned data
-    $: artists = $query.data || {};
 </script>
 
-{#if $query.isLoading}
-    <p>{$_("text.loading")}</p>
-{:else if $query.isError}
-    <p>Error: {$query.error.message}</p>
-{:else if $query.isSuccess}
-    {#if artists.length === 0}
-        <p>{$_("text.noItemsFound")}</p>
-    {:else}
-        <div class="lister-tabulator">
-            <div class="lister-tabulator__actions">
-                <Actions
-                    type="songs"
-                    displayMode="fullButtons"
-                    showShuffle={artists.length > 1}
-                    data={Object.create({ artists: artists })}
-                />
+<div class="lister-tabulator">
+    <div class="lister-tabulator__actions">
+        <MassRater bind:tabulator type="artist" />
+    </div>
 
-                <MassRater bind:tabulator type="artist" />
-            </div>
+    <Tabulator
+        bind:tabulator
+        columns={artistsPreset}
+        options={{
+            ...remotePaginationDefaults,
+            ajaxConfig: {
+                mode: "cors",
+                method: "GET", //set request type to Position
+                headers: {
+                    Authorization: "Bearer " + $User.token, //set specific content type
+                },
+            },
+            ajaxURLGenerator: function (url, config, params) {
+                if (params.size === true) {
+                    params.size = 0;
+                }
 
-            <Tabulator
-                bind:tabulator
-                data={artists}
-                columns={artistsPreset}
-                options={{ persistenceID: "artists" }}
-            ></Tabulator>
-        </div>
-    {/if}
-{/if}
+                return $API.rawURL("stats", {
+                    type: "artist",
+                    filter: "flagged",
+                    sort: "user_flag_rating,DESC",
+                    limit: params.size,
+                    offset: (params.page - 1) * params.size,
+                });
+            },
+            ajaxResponse: function (url, params, response) {
+                return normalizeResponse("artist", url, params, response);
+            },
+            persistenceID: "artists",
+        }}
+    ></Tabulator>
+</div>
