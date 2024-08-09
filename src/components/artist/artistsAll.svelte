bd9cffc3f087b586200d6c04681f07f54faad683
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2024-08-09 12:58:35 +1000

    Mass rater for artists views

--- src/components/artist/artistsAll.svelte
+++ src/components/artist/artistsAll.svelte
@@ -1,6 +1,7 @@
 <script>
     import { API, User } from "~/stores/state";
     import Tabulator from "~/components/lister/Tabulator.svelte";
+    import MassRater from "~/components/lister/massRater.svelte";
     import {
         artistsPreset,
         normalizeResponse,
@@ -13,33 +14,39 @@
     let tabulator = null;
 </script>
 
-<Tabulator
-    bind:tabulator
-    columns={artistsPreset}
-    options={{
-        ...remotePaginationDefaults,
-        ajaxConfig: {
-            mode: "cors",
-            method: "GET", //set request type to Position
-            headers: {
-                Authorization: "Bearer " + $User.token, //set specific content type
-            },
-        },
-        ajaxURLGenerator: function (url, config, params) {
-            if (params.size === true) {
-                params.size = 0;
-            }
+<div class="lister-tabulator">
+    <div class="lister-tabulator__actions">
+        <MassRater bind:tabulator type="artist" />
+    </div>
 
-            return $API.rawURL("artists", {
-                album_artist: type === "album_artist" ? 1 : 0,
-                sort: "basename,ASC",
-                limit: params.size,
-                offset: (params.page - 1) * params.size,
-            });
-        },
-        ajaxResponse: function (url, params, response) {
-            return normalizeResponse("artist", url, params, response);
-        },
-        persistenceID: "artists",
-    }}
-></Tabulator>
+    <Tabulator
+        bind:tabulator
+        columns={artistsPreset}
+        options={{
+            ...remotePaginationDefaults,
+            ajaxConfig: {
+                mode: "cors",
+                method: "GET", //set request type to Position
+                headers: {
+                    Authorization: "Bearer " + $User.token, //set specific content type
+                },
+            },
+            ajaxURLGenerator: function (url, config, params) {
+                if (params.size === true) {
+                    params.size = 0;
+                }
+
+                return $API.rawURL("artists", {
+                    album_artist: type === "album_artist" ? 1 : 0,
+                    sort: "basename,ASC",
+                    limit: params.size,
+                    offset: (params.page - 1) * params.size,
+                });
+            },
+            ajaxResponse: function (url, params, response) {
+                return normalizeResponse("artist", url, params, response);
+            },
+            persistenceID: "artists",
+        }}
+    ></Tabulator>
+</div>
