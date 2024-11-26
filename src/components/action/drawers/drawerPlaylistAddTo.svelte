07636b55c9a933d51f35700a37126488e0ce8321
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2024-11-26 22:19:27 +1100

    Fix playlist selection

--- src/components/action/drawers/drawerPlaylistAddTo.svelte
+++ src/components/action/drawers/drawerPlaylistAddTo.svelte
@@ -11,7 +11,7 @@
     let drawer = $state(),
         drawerEdit = $state();
     let newPlaylist = $state(null);
-    let selectedPlaylists = $state();
+    let selectedPlaylists = $state([]);
     let ignoreDuplicates = $state(false);
 
     function handleSave() {
