effa3774c6f0ee78fe0e0159c33017be0127d2b6
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2024-11-29 08:43:17 +1100

    Sidebar toggles test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -6,3 +6,12 @@ test("is logged in", async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Home/);
 });
+
+test('sidebar toggles', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByRole('link', { name: 'person_outline Artists' })).not.toBeVisible();
+    await page.getByText('library_music').click();
+    await expect(page.getByRole('link', { name: 'person_outline Artists' })).toBeVisible();
+    await page.getByText('library_music').click();
+    await expect(page.getByRole('link', { name: 'person_outline Artists' })).not.toBeVisible();
+  });
