8ea7977cdb0f282f1ec9a3bc9c5fbbc4280c4132
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2024-11-29 12:50:07 +1100

    home page tests

--- e2e/general.spec.ts
+++ e2e/general.spec.ts
@@ -1,10 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-test("is logged in", async ({ page }) => {
+test("homepage", async ({ page }) => {
     await page.goto("/");
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Home/);
+
+    await expect(
+        page.getByRole("heading", { name: "Newest Albums" }),
+    ).toBeVisible();
+
+    await expect(
+        page.getByRole("heading", { name: "Artist Mixes" }),
+    ).toBeVisible();
+
+    await expect(
+        page.getByRole("heading", { name: "Recently Played" }),
+    ).toBeVisible();
+
+    await expect(
+        page.getByRole("heading", { name: "Random Albums" }),
+    ).toBeVisible();
 });
 
 test("sidebar toggles", async ({ page }) => {
