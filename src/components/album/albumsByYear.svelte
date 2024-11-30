67ed315a8080c0fc96a4470b6ec456ca02cd0f34
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2024-11-30 17:58:09 +1100

    Runify albumsByYear

--- src/components/album/albumsByYear.svelte
+++ src/components/album/albumsByYear.svelte
@@ -1,8 +1,6 @@
 <script>
-    import { run } from "svelte/legacy";
-
     import { getAlbumsByYear } from "~/logic/album.js";
-    import YearPagination from "~/components/yearPagination.svelte";
+    import YearPagination from "~/components/yearSelector.svelte";
     import Actions from "~/components/action/actions.svelte";
     import Tabulator from "~/components/lister/Tabulator.svelte";
     import MassRater from "~/components/lister/massRater.svelte";
@@ -10,24 +8,23 @@
 
     let { showYear = new Date().getFullYear() } = $props();
 
-    let dataDisplay = $state([]);
     let fromYear = $state(null);
     let toYear = $state(null);
-    let loadedTime = $state(0);
-    let tabulator = $state(null);
+    let tabulator = $state();
 
     async function getData() {
+        tabulator?.setData([]);
+
         let response = await getAlbumsByYear({
             limit: 5000,
             from: fromYear,
             to: toYear,
         });
 
-        dataDisplay = response.album || [];
-
-        loadedTime = new Date();
+        tabulator?.setData(response.album || []);
     }
-    run(() => {
+
+    $effect(() => {
         // YearPagination 'Search' button just passes the final value, so get data if the years change
         if (fromYear && toYear) {
             getData();
@@ -37,30 +34,24 @@
 
 <YearPagination bind:fromYear bind:toYear {showYear} />
 
-{#if fromYear && toYear}
-    {#key fromYear + toYear}
-        {#key loadedTime}
-            <div class="lister-tabulator">
-                <div class="lister-tabulator__actions">
-                    <Actions
-                        type="year"
-                        displayMode="fullButtons"
-                        showShuffle={true}
-                        data={{ from: fromYear, to: toYear }}
-                    />
+<div class="lister-tabulator">
+    <div class="lister-tabulator__actions">
+        <Actions
+            type="year"
+            displayMode="fullButtons"
+            showShuffle={true}
+            data={{ from: fromYear, to: toYear }}
+        />
 
-                    <MassRater bind:tabulator type="album" />
-                </div>
+        <MassRater bind:tabulator type="album" />
+    </div>
 
-                <Tabulator
-                    bind:tabulator
-                    data={dataDisplay}
-                    columns={albumsPreset}
-                    options={{
-                        persistenceID: "albums",
-                    }}
-                ></Tabulator>
-            </div>
-        {/key}
-    {/key}
-{/if}
+    <Tabulator
+        bind:tabulator
+        data={[]}
+        columns={albumsPreset}
+        options={{
+            persistenceID: "albums",
+        }}
+    ></Tabulator>
+</div>
