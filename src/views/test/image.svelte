191ae4efe4dbec36879cf68aea81b8570e7a3651
Mitch Ray <5735900+mitchray@users.noreply.github.com>
2025-05-30 20:39:51 +1000

    Update image test

--- src/views/test/image.svelte
+++ src/views/test/image.svelte
@@ -4,7 +4,15 @@
 
 <h2>Image</h2>
 
-{#await $API.getArt({ type: "album", id: 1935, size: "128x128" })}
+{#await $API.getArt({ type: "album", id: 1935, size: "275x275" })}
+    <p>Loading</p>
+{:then blob}
+    <img src={URL.createObjectURL(blob)} />
+{:catch error}
+    <p>An error occurred!: {error}</p>
+{/await}
+
+{#await $API.getArt({ type: "artist", id: 1540, size: "275x275" })}
     <p>Loading</p>
 {:then blob}
     <img src={URL.createObjectURL(blob)} />
